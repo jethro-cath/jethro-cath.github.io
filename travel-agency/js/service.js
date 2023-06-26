52ab9987cbec287ce8f8c8342d2e399ea6e04084
Some real name =) <kaynya@gmail.com>
2023-06-26 14:37:38 +0300

    Burger opening has been added

--- travel-agency/js/service.js
+++ travel-agency/js/service.js
@@ -1,17 +1,28 @@
 'use strict';
 
-let bookingForm = document.querySelector('.hero__booking-form');
-let container = getComputedStyle(document.querySelector('.hero__title'));
-let padding = 0;
+const burgerButton = document.querySelector('.page-header__burger');
+const closeButton = document.querySelector('.page-header__close');
+const headerMenu = document.querySelector('.page-header');
 
-function paddingChanger() {
-    let num1 = container.marginLeft;
-    console.log(num1);
-    let num2 = container.paddingLeft;
-    padding = parseInt(num1) + parseInt(num2);
-    bookingForm.style.paddingLeft = padding + 'px';
+burgerButton.onclick = function() {
+    headerMenu.classList.add('page-header-mobile');
 }
 
-paddingChanger();
-window.addEventListener("resize", paddingChanger);
-// window.onresize = paddingChanger();
+closeButton.onclick = function() {
+    headerMenu.classList.remove('page-header-mobile')
+    };
+
+// let bookingForm = document.querySelector('.hero__booking-form');
+// let container = getComputedStyle(document.querySelector('.hero__title'));
+// let padding = 0;
+
+// function paddingChanger() {
+//     let num1 = container.marginLeft;
+//     console.log(num1);
+//     let num2 = container.paddingLeft;
+//     padding = parseInt(num1) + parseInt(num2);
+//     bookingForm.style.paddingLeft = padding + 'px';
+// }
+
+// paddingChanger();
+// window.addEventListener("resize", paddingChanger);
